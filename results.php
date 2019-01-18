6082f1b8610b82f443e8fb414ac0803df6ca5ac7
Sebastian Johansson <sebben.97@hotmail.com>
2019-01-18 11:21:54 +0100

    made an alert for each animal sound.

--- results.php
+++ results.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="./style.css">
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <body>
     
 <?php
@@ -9,7 +10,7 @@
     $koko = $_POST['kokos'];
 
     for($i = 0; $i < $nrOfApor; $i++){
-        $apa = new Apa("Herbert", "monkey");
+        $apa = new Apa("Herbert");
         echo $apa->image;
             
     }
@@ -28,5 +29,46 @@
         echo $kokos->image;
             
     }
+    $apaSound = $apa->makeSound();
+    $tigerSound = $tiger->makeSound();
+    $giraffSound = $giraff->makeSound();
+    $kokoSound = $kokos->makeSound();
+    
+
+    
 ?>
+<script type="text/javascript">
+    $(document).ready(function(){
+        var apa = <?php echo json_encode($apaSound);?>;
+        var tiger = <?php echo json_encode($tigerSound);?>;
+        var giraff = <?php echo json_encode($giraffSound);?>;
+        var kokos = <?php echo json_encode($kokoSound);?>;
+
+    
+    $(".apa").on("click", function(){ monkeySound(); });
+
+    function monkeySound(){
+        alert(apa);
+    }
+
+    $(".tiger").on("click", function(){ tigerSound(); });
+
+    function tigerSound(){
+        alert(tiger);
+    }
+
+    $(".giraff").on("click", function(){ giraffSound(); });
+
+    function giraffSound(){
+        alert(giraff);
+    }
+
+    $(".coco").on("click", function(){ kokoSound(); });
+
+    function kokoSound(){
+        alert(kokos);
+    }
+
+});
+</script>
 </body>
